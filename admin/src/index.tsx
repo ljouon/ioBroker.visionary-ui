df8378d851e645b4625d554b60dbb4bf38e57948
ljouon <schm.lars@googlemail.com>
2023-11-22 10:27:12 +0100

    chore: upgrade and migrate to adapter-react-v5

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import theme from '@iobroker/adapter-react/Theme';
-import Utils from '@iobroker/adapter-react/Components/Utils';
+import Utils from '@iobroker/adapter-react-v5/Components/Utils';
 import App from './app';
+import { StyledEngineProvider, ThemeProvider } from '@mui/material';
+
+import theme from '@iobroker/adapter-react-v5/Theme';
 
 let themeName = Utils.getThemeName();
 
 function build(): void {
     ReactDOM.render(
-        <MuiThemeProvider theme={theme(themeName)}>
-            <App
-                adapterName="visionary-ui"
-                onThemeChange={(_theme) => {
-                    themeName = _theme;
-                    build();
-                }}
-            />
-        </MuiThemeProvider>,
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme(themeName)}>
+                <App
+                    adapterName="visionary-ui"
+                    onThemeChange={(_theme) => {
+                        themeName = _theme;
+                        build();
+                    }}
+                />
+            </ThemeProvider>
+        </StyledEngineProvider>,
         document.getElementById('root'),
     );
 }
