a059d9974a472cde8a96bd4abd9f4ede1ab0b302
Lars Jouon <lars.jouon@codecentric.de>
2023-12-09 00:46:10 +0100

    chore(client): add simple menu and sidebar

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -9,44 +9,47 @@ interface SidebarProps<T extends VuiEnum> extends React.HTMLAttributes<HTMLDivEl
 }
 
 export function Sidebar<T extends VuiEnum>({ className, treeNodes }: SidebarProps<T>) {
+    function hasStateObjects<T extends VuiEnum>(node: TreeNode<T>) {
+        return node.data && node.data.members ? node.data.members.length > 0 : false;
+    }
+
+    function hasChildren<T>(node: TreeNode<T>) {
+        return node.children.length > 0;
+    }
+
     return (
         <div className={cn('pb-12', className)}>
             <div className="space-y-4 py-4">
                 {treeNodes?.map((node) => {
-                    return (
-                        <div key={node.canonicalPath} className="py-2">
-                            <h2 className="relative px-7 text-lg font-semibold tracking-tight">{node.data?.name}</h2>
-                            <ScrollArea className=" px-1">
-                                <div className="space-y-1 p-2">
-                                    {node.children?.map((child, i) => (
-                                        <Button
-                                            key={`${child}-${i}`}
-                                            variant="ghost"
-                                            className="w-full justify-start font-normal"
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeWidth="2"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                className="mr-2 h-4 w-4"
-                                            >
-                                                <path d="M21 15V6" />
-                                                <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                                                <path d="M12 12H3" />
-                                                <path d="M16 6H3" />
-                                                <path d="M12 18H3" />
-                                            </svg>
-                                            {child.data?.name}
-                                        </Button>
-                                    ))}
-                                </div>
-                            </ScrollArea>
-                        </div>
-                    );
+                    if (hasChildren(node)) {
+                        return (
+                            <div key={node.canonicalPath} className="py-1">
+                                <h2 className="relative px-7 text-lg font-semibold tracking-tight">
+                                    {node.data?.name}
+                                </h2>
+                                <ScrollArea className="px-1">
+                                    <div className="space-y-1 p-1">
+                                        {node.children?.map((child, i) => {
+                                            if (hasStateObjects(child)) {
+                                                return (
+                                                    <Button
+                                                        key={`${child}-${i}`}
+                                                        variant="ghost"
+                                                        className="w-full justify-start font-normal"
+                                                    >
+                                                        <div className={'w-8 pr-2'}>
+                                                            <img src={child.data?.icon ?? undefined} alt={'icon'} />
+                                                        </div>
+                                                        {child.data?.name}
+                                                    </Button>
+                                                );
+                                            }
+                                        })}
+                                    </div>
+                                </ScrollArea>
+                            </div>
+                        );
+                    }
                 })}
             </div>
         </div>
