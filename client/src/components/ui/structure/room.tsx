38dd8eacba284fb0d4c566deb5f3b4311cf36a35
Lars Jouon <lars.jouon@codecentric.de>
2023-12-12 17:01:07 +0100

    chore(client): card content stays within card now

--- client/src/components/ui/structure/room.tsx
+++ client/src/components/ui/structure/room.tsx
@@ -22,13 +22,10 @@ export function Room({id, title, icon, sub}: RoomProps) {
                     <span className="ml-2">{title}</span></h1>
             </div>
             <div className="gap-6 rounded-lg p-8 lg:columns-2 xl:columns-3 space-y-6">
-                {/*<div className="h-full px-4 py-6 sm:px-8">*/}
-                {/*    <div className="flex items-center justify-center [&>div]:w-full">*/}
                 {sub.map(element => {
                     return <FunctionCard title={element.name} key={element.id} id={element.id} roomId={id}
                                          objects={element.members ?? []}/>
                 })}
-                {/*</div>*/}
             </div>
         </>
     );
