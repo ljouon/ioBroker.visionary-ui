a2729fefb70bbfeb014921e7ca9bf87691e6bb59
Lars Jouon <lars.jouon@codecentric.de>
2023-12-12 12:01:08 +0100

    chore(client): set up responsive grid cols

--- client/src/components/ui/structure/room.tsx
+++ client/src/components/ui/structure/room.tsx
@@ -21,22 +21,13 @@ export function Room({id, title, icon, sub}: RoomProps) {
                     />
                     <span className="ml-2">{title}</span></h1>
             </div>
-            <div
-                className="items-start justify-center gap-6 rounded-lg p-8 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+            <div className="gap-6 rounded-lg p-8 lg:columns-2 xl:columns-3 space-y-6">
                 {/*<div className="h-full px-4 py-6 sm:px-8">*/}
                 {/*    <div className="flex items-center justify-center [&>div]:w-full">*/}
                 {sub.map(element => {
                     return <FunctionCard title={element.name} key={element.id} id={element.id} roomId={id}
                                          objects={element.members ?? []}/>
                 })}
-                {/*<FunctionCard title='Licht' id='light' roomId={id}/>*/}
-                {/*<FunctionCard title='Heizung' id='heating' roomId={id}/>*/}
-                {/*<FunctionCard title='Fenster' id='windows' roomId={id}/>*/}
-                {/*<FunctionCard title='Sicherheit' id='security' roomId={id}/>*/}
-                {/*<FunctionCard title='Sonstiges' id='other' roomId={id}/>*/}
-                {/*<FunctionCard title='Wetter' id='weather' roomId={id}/>*/}
-                {/*<FunctionCard title='Systeme' id='systems' roomId={id}/>*/}
-                {/*<FunctionCard title='Waschen' id='wasching' roomId={id}/>*/}
                 {/*</div>*/}
             </div>
         </>
