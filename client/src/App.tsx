4009df82a1ea4683acd3e612e461d2221f5dba3e
Lars Jouon <lars.jouon@codecentric.de>
2023-12-20 17:54:08 +0100

    chore: switch between aspects on card title click, use some smaller paddings, update todo in README.md

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,8 @@
 import './App.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {routes} from "@/app/routes";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from '@/app/routes';
 
 export function App() {
-
-
     const router = createBrowserRouter(routes);
-
-    /*{pageContent}*/
-
-
-    return (
-        <RouterProvider router={router}/>
-    );
+    return <RouterProvider router={router} />;
 }
