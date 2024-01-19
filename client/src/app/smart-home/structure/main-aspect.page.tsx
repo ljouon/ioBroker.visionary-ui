c7c43d2fa783a4b54c353cafc9faec5a01b8f054
ljouon <schm.lars@googlemail.com>
2024-01-19 20:52:30 +0100

    chore(client): add scroll to top button

--- client/src/app/smart-home/structure/main-aspect.page.tsx
+++ client/src/app/smart-home/structure/main-aspect.page.tsx
@@ -1,18 +1,22 @@
-import {useParams} from 'react-router-dom';
-import {AspectNode, findAspectNode} from '@/app/smart-home/structure/aspect';
-import {useVuiDataContext} from '@/app/smart-home/data.context';
-import {MainAspectSection} from '@/app/smart-home/structure/main-aspect.section';
-import {hasStateObjects, isRoom, VuiEnum} from '../../../../../src/domain';
-import {matchPath} from '@/app/route-utils';
-import {useEffect, useState} from 'react';
-import {ErrorDisplay} from '@/app/components/error';
-import {useTranslation} from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { AspectNode, findAspectNode } from '@/app/smart-home/structure/aspect';
+import { useVuiDataContext } from '@/app/smart-home/data.context';
+import { MainAspectSection } from '@/app/smart-home/structure/main-aspect.section';
+import { hasStateObjects, isRoom, VuiEnum } from '../../../../../src/domain';
+import { matchPath } from '@/app/route-utils';
+import { useEffect, useState } from 'react';
+import { ErrorDisplay } from '@/app/components/error';
+import { useTranslation } from 'react-i18next';
+import { DynamicMaterialDesignIcon } from '@/app/components/dynamic-material-design-icon';
 
 export function MainAspectPage() {
-    const {mainAspect, canonicalPath} = useParams();
-    const {roomAspectNodes, functionAspectNodes, connectionState} = useVuiDataContext();
+    const { mainAspect, canonicalPath } = useParams();
+    const { roomAspectNodes, functionAspectNodes, connectionState } = useVuiDataContext();
+
     const [selectedAspectNode, setSelectedAspectNode] = useState<AspectNode | null>(null);
-    const {t} = useTranslation();
+    const [topButtonVisible, setTopButtonVisible] = useState<boolean>(false);
+
+    const { t } = useTranslation();
 
     useEffect(() => {
         if (mainAspect && roomAspectNodes && functionAspectNodes) {
@@ -46,14 +50,36 @@ export function MainAspectPage() {
         );
     }
 
+    window.addEventListener('scroll', () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            setTopButtonVisible(true);
+        } else {
+            setTopButtonVisible(false);
+        }
+    });
+
     const mainAspects = getMainAspects(selectedAspectNode);
-    return mainAspects.map((element, index) => (
-        <MainAspectSection
-            key={`${index}_${element.id}`}
-            id={element.id}
-            type={isRoom(element) ? 'rooms' : 'functions'}
-        />
-    ));
+    return (
+        <>
+            {mainAspects.map((element, index) => (
+                <MainAspectSection
+                    key={`${index}_${element.id}`}
+                    id={element.id}
+                    type={isRoom(element) ? 'rooms' : 'functions'}
+                />
+            ))}
+            <div
+                className={`${
+                    topButtonVisible ? '' : 'hidden'
+                } !fixed right-0 bottom-8 rounded-l-full bg-gray-400 hover:bg-primary cursor-pointer inline-flex items-center justify-center`}
+                onClick={() => {
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                }}
+            >
+                <DynamicMaterialDesignIcon iconKey="arrow-up-circle" className={`h-10 w-10 text-primary-foreground`} />
+            </div>
+        </>
+    );
 }
 
 function getMainAspects(node: AspectNode): VuiEnum[] {
